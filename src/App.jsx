ade3697c5af99dee053d98e889b659b834661b3a
kunal-rp <kunal@KRP-Silver.local>
2023-07-03 16:35:27 -0700

    section + sticky setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 
 function Box(props) {
@@ -25,16 +25,83 @@ function Box(props) {
   );
 }
 
-export default function App(props) {
+function Books(props) {
   return (
-    <>
-      <Canvas style={{ width: "100%", height: "100%" }}>
-        <ambientLight />
-        <pointLight position={[10, 10, 10]} />
-        <Box position={[-1.2, 0, 0]} />
-        <Box position={[1.2, 0, 0]} />
-      </Canvas>
-      ,
-    </>
+    <Canvas
+      style={{ width: "100vw", height: "100vh", background: props.color }}
+    >
+      <ambientLight />
+      <pointLight position={[10, 10, 10]} />
+      <Box position={[-1.2, 0, 0]} />
+      <Box position={[1.2, 0, 0]} />
+    </Canvas>
+  );
+}
+
+function ProgressIndicator(props) {
+  const [per, setPer] = useState(0);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      var containerRect = props.containerRef.current.getBoundingClientRect();
+
+      console.log(containerRect.top + window.scrollY);
+      var winScroll = document.documentElement.scrollTop - containerRect.top;
+      var height = containerRect.height;
+      var scrolled = (winScroll / height) * 100;
+      setPer(scrolled);
+    });
+  }, []);
+  return <>{per}</>;
+}
+
+const SECTION_POSITIONS = [
+  {
+    fillerSize: "200vh",
+    element: <Books color={"red"} />,
+  },
+  {
+    element: <div style={{ background: "green", height: "10vh" }} />,
+  },
+  {
+    fillerSize: "200vh",
+    element: <Books color={"blue"} />,
+  },
+  {
+    element: <div style={{ background: "green", height: "10vh" }} />,
+  },
+  {
+    fillerSize: "200vh",
+    element: <Books color={"yellow"} />,
+  },
+];
+
+function Section(props) {
+  const containerRef = useRef();
+
+  return (
+    <div className="container" ref={containerRef}>
+      <div className="content">
+        {" "}
+        {React.cloneElement(props.element, {
+          containerRef: containerRef,
+        })}
+      </div>
+      <div className="filler" style={{ height: props.fillerSize ?? "0px" }} />
+    </div>
+  );
+}
+
+export default function App(props) {
+  const containerRef1 = useRef();
+  const containerRef2 = useRef();
+  const containerRef3 = useRef();
+
+  return (
+    <div>
+      {SECTION_POSITIONS.map((section) => (
+        <Section fillerSize={section.fillerSize} element={section.element} />
+      ))}
+    </div>
   );
 }
